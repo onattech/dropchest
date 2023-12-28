4a757cae3f08607505be746c21accba1d6f16b58
Onat <53895969+onattech@users.noreply.github.com>
2023-12-28 22:43:11 +0300

    Added Clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image"
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="">
             <h1>ChestBox</h1>
+            <UserButton afterSignOutUrl="/" />
         </main>
     )
 }
